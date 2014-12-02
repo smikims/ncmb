13ff4a3fe707d4c10d29b907203839f8f6d290f1
smikims <smikims.me@gmail.com>
2014-12-02 16:57:30 -0600

    fixed zoom box thingy

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 CC	:= cc
 SRC	:= mandel.c
-CFLAGS	:= -Wall -pedantic-errors -ansi -lm -lcurses -std=c11
+CFLAGS	:= -Wall -Wextra -pedantic-errors -ansi -lm -lcurses -std=c11
 DFLAGS	:= -g -pg -O0
 INSTALL	:= /usr/local/bin/ncmb
 OUT	:= ncmb
--- ncmb.c
+++ ncmb.c
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <locale.h>
+#include <string.h>
 
 struct zoom_stack {
 	struct zoom_level {
@@ -35,10 +36,13 @@ int iter = 100;
 int bailout = 2;
 double power = 2;
 char colors[] = " .:~#";
+int big_scroll_amt_x = 20;
+int big_scroll_amt_y = 15;
 
 struct zoom_stack *zstack = NULL;
 
-int iter_color(double complex c, int max_iter, int ncolors)
+int
+iter_color(double complex c, int max_iter, int ncolors)
 {
 	double complex z = 0;
 	for (int i = 0; i < max_iter; ++i) {
@@ -50,60 +54,73 @@ int iter_color(double complex c, int max_iter, int ncolors)
 	return 0; // shut up the compiler; we'll never get here
 }
 
-int iter_color_generic(double complex c, double power, int max_iter, int ncolors)
+int
+iter_color_generic(double complex c, int max_iter, int ncolors)
 {
 	double complex z = 0;
 	for (int i = 0; i < max_iter; ++i) {
-		z = cpow(z,power) + c;
+		z = cpow(z, power) + c;
 		if (cabs(z) > bailout || i + 1 == max_iter)
 			return i % ncolors;
 	}
 
-	return 0; // shut up the compiler; we'll never get here
+	return 0;
 }
 
-double complex complex_value(int i, int j)
+double complex
+complex_value(int i, int j)
 {
 	return (xmin + j*xstep) + (ymax - i*ystep)*I;
 }
 
-void draw(void)
+void
+draw(void)
 {
+	// Yes, equality with floats is evil, but it's OK becuase we only need
+	// to worry about one value, which I've checked works.
+	int (*color_alg)(double complex, int, int) = power == 2 ? iter_color : iter_color_generic;
+	int ncolors = strlen(colors);
+
 	getmaxyx(stdscr, height, width);
 	for (int i = 0; i < height; ++i) {
 		for (int j = 0; j < width; ++j) {
-			mvaddch(i, j, colors[iter_color(complex_value(i, j), iter, sizeof(colors) - 1)]);
+			mvaddch(i, j, colors[color_alg(complex_value(i, j), iter, ncolors)]);
 		}
 	}
 
 	refresh();
 }
 
-void scrollup(void)
+void
+scrollup(void)
 {
 	ymin += ystep;
 	ymax += ystep;
 }
 
-void scrolldown(void)
+void
+scrolldown(void)
 {
 	ymin -= ystep;
 	ymax -= ystep;
 }
 
-void scrollleft(void)
+void
+scrollleft(void)
 {
 	xmin -= xstep;
 	xmax -= xstep;
 }
 
-void scrollright(void)
+void
+scrollright(void)
 {
 	xmin += xstep;
 	xmax += xstep;
 }
 
-void zoom_stack_push(struct zoom_stack **stk, struct zoom_level lvl)
+void
+zoom_stack_push(struct zoom_stack **stk, struct zoom_level lvl)
 {
 	struct zoom_stack *newstk = malloc(sizeof(struct zoom_stack));
 	newstk->zoom_level = lvl;
@@ -111,7 +128,8 @@ void zoom_stack_push(struct zoom_stack **stk, struct zoom_level lvl)
 	*stk = newstk;
 }
 
-struct zoom_level *zoom_stack_pop(struct zoom_stack **stk)
+struct zoom_level *
+zoom_stack_pop(struct zoom_stack **stk)
 {
 	if (!stk || !(*stk)) return NULL;
 	struct zoom_level *ret = &((*stk)->zoom_level); // guaranteed to be the funkiest
@@ -120,7 +138,8 @@ struct zoom_level *zoom_stack_pop(struct zoom_stack **stk)
 }
 
 // TODO: fix all the fancy zoom stuff
-void hilight_rect(int start_y, int start_x, int end_y, int end_x)
+void
+hilight_rect(int starty, int startx, int endy, int endx)
 {
 	for (int i = 0; i < height; ++i) {
 		for (int j = 0; j < width; ++j) {
@@ -128,24 +147,51 @@ void hilight_rect(int start_y, int start_x, int end_y, int end_x)
 		}
 	}
 
-	int ay = end_y > start_y ? start_y : end_y;
-	int by = end_y > start_y ? end_y : start_y;
-	int ax = end_x > start_x ? start_x : end_x;
-	int bx = end_x > start_x ? end_x : start_x;
+	int ay = endy > starty ? starty : endy;
+	int by = endy > starty ? endy : starty;
+	int ax = endx > startx ? startx : endx;
+	int bx = endx > startx ? endx : startx;
 	for (int i = ay; i <= by; ++i) {
 		for (int j = ax; j <= bx; ++j) {
 			mvchgat(i, j, 1, A_REVERSE, 0, NULL);
 		}
 	}
+
+	move(endy, endx);
 }
 
-void zoom(void)
+// TODO: fix the math
+void
+zoom_center(double zoom_factor)
+{
+	zoom_stack_push(&zstack, (struct zoom_level) { ymin, ymax, xmin, xmax });
+	ymin += (ymax - ymin) / (zoom_factor * 2);
+	ymax -= (ymax - ymin) / (zoom_factor * 2);
+	xmin += (xmax - xmin) / (zoom_factor * 2);
+	xmax -= (xmax - xmin) / (zoom_factor * 2);
+}
+
+void
+zoom_out(void)
+{
+	struct zoom_level *lvl = zoom_stack_pop(&zstack);
+	if (!lvl) return;
+
+	ymin = lvl->ymin;
+	ymax = lvl->ymax;
+	xmin = lvl->xmin;
+	xmax = lvl->xmax;
+
+	free(lvl);
+}
+
+void
+zoom(void)
 {
 	move(height / 2, width / 2);
 	refresh();
 
 	bool box_select = false;
-	double old_ymin = ymin, old_ymax = ymax, old_xmin = xmin, old_xmax = xmax;
 	int ch, y, x, box_start_y = 0, box_start_x = 0;
 	while (true) {
 		getyx(stdscr, y, x);
@@ -167,31 +213,37 @@ void zoom(void)
 		case 'l':
 			move(y, ++x);
 			break;
-		// TODO: this doesn't work right
-		case KEY_BACKSPACE:
+		case 'z':
+			return;
+			break;
+		case 'q':
+			ungetch('q'); // Quit when we get back to main()
 			return;
 			break;
 		case ' ':
 			if (box_select) {
-				ymin = cimag(y > box_start_y ?
+				double new_ymin, new_xmin, new_ymax, new_xmax;
+				zoom_stack_push(&zstack, (struct zoom_level) { ymin, ymax, xmin, xmax });
+
+				new_ymin = cimag(y > box_start_y ?
 						complex_value(y, x) :
 						complex_value(box_start_y, box_start_x));
-				xmin = creal(x > box_start_x ?
+				new_xmin = creal(x > box_start_x ?
 						complex_value(box_start_y, box_start_x) :
 						complex_value(y, x));
-				ymax = cimag(y > box_start_y ?
+				new_ymax = cimag(y > box_start_y ?
 						complex_value(box_start_y, box_start_x) :
 						complex_value(y, x));
-				xmax = creal(x > box_start_x ?
+				new_xmax = creal(x > box_start_x ?
 						complex_value(y, x) :
 						complex_value(box_start_y, box_start_x));
 
-				if (xmax - xmin == 0 || ymax - ymin == 0) {
-					ymin = old_ymin; ymax = old_ymax;
-					xmin = old_xmin; xmax = old_xmax;
+				if (!(xmax - xmin == 0) && !(ymax - ymin == 0)) {
+					ymin = new_ymin; ymax = new_ymax;
+					xmin = new_xmin; xmax = new_xmax;
 				} else {
 					// TODO: this doesn't seem to work right
-					zoom_stack_push(&zstack, (struct zoom_level) { ymin, ymax, xmin, xmax });
+					zoom_out();
 				}
 
 				return;
@@ -209,31 +261,12 @@ void zoom(void)
 	}
 }
 
-// TODO: fix the math
-void zoom_center(double zoom_factor)
+int
+main(int argc, char *argv[])
 {
-	zoom_stack_push(&zstack, (struct zoom_level) { ymin, ymax, xmin, xmax });
-	ymin += (ymax - ymin) / (zoom_factor * 2);
-	ymax -= (ymax - ymin) / (zoom_factor * 2);
-	xmin += (xmax - xmin) / (zoom_factor * 2);
-	xmax -= (xmax - xmin) / (zoom_factor * 2);
-}
+	(void)argc; // get rid of -Wextra warning
+	(void)argv;
 
-void zoom_out(void)
-{
-	struct zoom_level *lvl = zoom_stack_pop(&zstack);
-	if (!lvl) return;
-
-	ymin = lvl->ymin;
-	ymax = lvl->ymax;
-	xmin = lvl->xmin;
-	xmax = lvl->xmax;
-
-	free(lvl);
-}
-
-int main(int argc, char *argv[])
-{
 	// Various options everyone uses with curses
 	setlocale(LC_ALL, "");
 	initscr();
@@ -264,6 +297,22 @@ int main(int argc, char *argv[])
 		case KEY_RIGHT:
 		case 'l':
 			scrollright();
+			break;
+		case 'H':
+			for (int i = 0; i < big_scroll_amt_x; ++i)
+				scrollleft();
+			break;
+		case 'J':
+			for (int i = 0; i < big_scroll_amt_y; ++i)
+				scrolldown();
+			break;
+		case 'K':
+			for (int i = 0; i < big_scroll_amt_y; ++i)
+				scrollup();
+			break;
+		case 'L':
+			for (int i = 0; i < big_scroll_amt_x; ++i)
+				scrollright();
 			break;
 		case '+':
 		case '=':
